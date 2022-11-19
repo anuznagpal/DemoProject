b59d2b56ccfeb062cda88a3597f835cfd3f8a397
Anuz Nagpal <anuznagpal@Anuzs-Air.mshome.net>
2022-11-19 22:01:59 +0530

    Hello World

--- First.java
+++ First.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class First{
     public static void main(String args[]){
-        System.out.println("Hello World");
-        
+        List<Integer> ls = new ArrayList<>();
+        for(int i=0;i<30;i++){
+            ls.add(i);
+        }
+        ListIterator<Integer> iter = ls.listIterator();
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
     }
 }
